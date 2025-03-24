10290e4e42d9b8ed429f8495a2f07190fbbffae6
Tolga ERSOY <73739843+tolgaersoy07@users.noreply.github.com>
2025-03-24 19:51:19 +0300

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,7 @@
+app_config_secret_key="SECRET_KEY_UNIQUE_AND_HIGHLY_SECURE"
+
+SECRET_KEY = "SECRET_KEY_UNIQUE_AND_HIGHLY_SECURE"
+
+FERNET_ENCRYPTION_KEY="FERNET_ENCRYPTION_KEY_UNIQUE_AND_HIGHLY_SECURE"
+
+db_info={'host':'localhost','user':'root','password':'root123','database':'db_name'}
